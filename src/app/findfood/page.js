22c856d4c38e9e26eb81e28cea5f8dbf855f3117
Michael Slu <slumichael83@gmail.com>
2024-02-21 05:28:35 -0800

    - Revamped displaying and searching of restaurants and recipes - Fixed and implemented loading

--- src/app/findfood/page.js
+++ src/app/findfood/page.js
@@ -1,5 +1,4 @@
 'use client'
-
 import { useState, useEffect } from 'react';
 import Checkbox from '@mui/material/Checkbox';
 import IconButton from '@mui/material/IconButton';
@@ -14,167 +13,85 @@ import { TextField } from '@mui/material';
 import CircularProgress from '@mui/material/CircularProgress';
 import { recipes } from '../../../prisma/recipes';
 import { restaurants } from '../../../prisma/restaurants';
+import { Favorite } from '@mui/icons-material';
 
 export default function findFoods() {
 
     const [searchInput, setSearchInput] = useState('');
-    const [foods, setFoods] = useState({recipes: [], restaurants: []});
-    let result = [];
+    const [foods, setFoods] = useState([]);
+    const [results, setResults] = useState([]);
+    const [recipeIsLoading, setRecipeIsLoading] = useState(true);
+    const [restaurantIsLoading, setRestaurantIsLoading] = useState(true);
+    const [IsLoading, setIsLoading] = useState(true);
+    const loadingItems = <CircularProgress/>;
 
     const handler = async function() {
         const response_recipes = await fetch('/api/recipes');
         const recipes = await response_recipes.json();
+        setRecipeIsLoading(false)
         const response_restaurants = await fetch('/api/restaurants');
         const restaurants = await response_restaurants.json();
-        setFoods({restaurants: restaurants, recipes: recipes});
+        setRestaurantIsLoading(false)
+        setFoods(recipes.concat(restaurants));
+        setResults(recipes.concat(restaurants));
     }
-    const recipeList = foods.recipes?.map((recipe, index) => {
-        return(
-            <li key = {index}>
-            <h><b>{recipe.name}</b></h>
-            <div>{recipe.priceRange}</div>  
-            <br/>
-            </li>
-          );
-    });
-
-    const restaurantList = foods.restaurants?.map((restaurant, index) => {
-        return(
-            <li key = {index}>
-            <h><b>{restaurant.name}</b></h>
-            <div>{restaurant.priceRange}</div>
-            <br/>
-            </li>
-          );
-    });
-
+    
     useEffect(() => {
         handler();
     }, []);
 
-    const handleChange = (e) => {
-        e.preventDefault();
-        setSearchInput(e.target.value);
-    };
+    function doNothing(name){
+        return 1;
 
-    if (searchInput.length > 0) {
-        result.append(foods.recipes.filter((recipe) => 
-            recipe.name == searchInput));
-        result.append(foods.restaurants.filter((restaurant) => 
-            restaurant.name == searchInput));
     }
 
-    /*
-    const [todos, setTodos] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [newTodo, setNewTodo] = useState('');
+    function searchChanged(event){
+        setSearchInput(event.target.value);
+      }
 
-    function inputChangeHandler(e) {
-        setNewTodo(e.target.value);
+    function nameSearch(name, searchInput){
+        var found = false;
+        const inputNoSpaceAndLowerCase = (searchInput.replace(/\s+/g, '')).toLowerCase();
+        const splitInputLowerCase = searchInput.toLowerCase().split(' ')
+        const noSpaceAndLowerCase = (name.replace(/\s+/g, '')).toLowerCase();
+        splitInputLowerCase.forEach((word) => {
+            if (noSpaceAndLowerCase.includes(word)){
+                found = true;
+            }
+        })
+        return noSpaceAndLowerCase.includes(inputNoSpaceAndLowerCase) || found
     }
-
-    function addNewTodo() {
-        if(newTodo && newTodo.length) {
-            fetch("/api/todos", { method: "post", body: JSON.stringify({value: newTodo, done: false}) } ).then((response) => {
-                return response.json().then((newTodo) => {
-                    setTodos([...todos, newTodo]);
-                    setNewTodo('');
-                });
-            });
+    function searchFood() {
+        if (setResults.length && searchInput.length){
+            setResults(foods.filter((food) => nameSearch(food.name, searchInput)));
+        }
+        else{
+            setResults(foods)
         }
     }
-
-    function removeTodo({ index }) {
-        const todoItem = todos[index];
-        fetch(`/api/todos/${todoItem.id}`, { method: 'delete' }).then((res) => {
-            if (res.ok) {
-                setTodos(todos.filter((v,idx) => idx!==index));
-            }
-        });
-    }
-
-    function toggleDone({idx, item}) {
-        let updatedItem = {...item, done: !item.done};
-        fetch(`/api/todos/${item.id}}`, {method: 'put', body: JSON.stringify(updatedItem)}).then((res) => {
-            if(res.ok) {
-                const updatedTodos = [...todos];
-                updatedTodos[idx] = updatedItem;
-                setTodos(updatedTodos);
-            }
-        });
-    }
-
-    useEffect(() => {
-        fetch("/api/todos", { method: "get" }).then((response) => response.ok && response.json()).then(
-            todos => {
-                todos && setTodos(todos);
-                setIsLoading(false);
-            }
-        );
-    }, []);
-
-    const loadingItems = <CircularProgress/>;
-
-    const toDoItems = isLoading ? loadingItems : todos.map((todo, idx) => {
-        return <ListItem key={idx} secondaryAction={
-            <IconButton edge="end" onClick={() => removeTodo({index: idx})} aria-label='delete todo'><DeleteForever/></IconButton>   
+    const resultList = (restaurantIsLoading || recipeIsLoading) ? loadingItems: results.map((result, index) => {
+        return <ListItem key={index} secondaryAction={
+            <IconButton edge="end" onClick={() => doNothing(result.name)} aria-label='Do Nothing'><Favorite/></IconButton>   
         }>  
             <ListItemButton>
-                <ListItemIcon>
-                    <Checkbox checked={todo.done} disableRipple onChange={() => {
-                        toggleDone({idx, item: todo})
-                    }} aria-label="toggle done"/>
-                </ListItemIcon>
-                <ListItemText primary={todo.value}/>
+                <ListItemText primary={result.name}/>
+                <ListItemText primary={result.priceRange}/>
             </ListItemButton>
         </ListItem>;
-    });
-    
+    });    
 
-    return (
+    return(
+        <div>
+        <TextField label="Search For Food" fullWidth variant="outlined" value={searchInput} onChange={searchChanged}/> 
+        <button onClick={searchFood}>Search</button>
+        <div><h2>Meals</h2></div>
         <>
-            <List sx={{ width: '100%', maxWidth: 500 }}>
-                { toDoItems }
-                {!isLoading && <ListItem key="newItem" secondaryAction={<IconButton edge="end" onClick={addNewTodo} aria-label="add button"><AddBox/></IconButton>}>
-                    <TextField label="Search For Food" fullWidth variant="outlined" value={newTodo} onChange={inputChangeHandler}/> 
+            <List sx={{ width: '100%', maxWidth: 1500 }}>
+                { resultList }
+                {!(recipeIsLoading && restaurantIsLoading) && <ListItem key="food">
                 </ListItem>}
             </List>
         </>
-    );
-
-    */
-
-    return <div>
-
-        <TextField label="Search For Food" fullWidth variant="outlined" value={searchInput} onChange={handleChange}/> 
-        {result.map((food) => {
-        <div>
-            <ul>
-                <h2>{food.name}</h2>
-            </ul>
         </div>
-        /*
-        <input
-            type="search"
-            placeholder="Search here"
-             onChange={handleChange}
-            value={searchInput} />
-
-
-        {result.map((recipe) => {
-        <div>
-            <tr>
-                <td>{recipe.name}</td>
-            </tr>
-        </div>
-        */
-
-        }) }
-    <div>
-        <hr/>
-        <div>{recipeList}</div>
-        <hr/>
-        <div>{restaurantList}</div>
-    </div>
-    </div>
+    )
 }
