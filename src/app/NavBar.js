5489116c987098489df40c71f8906c0baaa57ab8
Michael Slu <117062577+mxsluu@users.noreply.github.com>
2024-02-22 15:48:45 -0800

    added account path to NavBar.js

--- src/app/NavBar.js
+++ src/app/NavBar.js
@@ -8,7 +8,8 @@ export default function NavBar() {
   const pathname = usePathname();
   const links = [
     { path: '/', name: 'Home' }, 
-    { path: '/todos', name: 'ToDos' }
+    { path: '/todos', name: 'ToDos' },
+    {path: '/account', name: 'Account'}
   ];
 
   return (
