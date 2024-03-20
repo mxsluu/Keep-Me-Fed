8579bc83a4bee70a9d4c9bd8e0096e861bc2bee0
Michael Slu <117062577+mxsluu@users.noreply.github.com>
2024-03-19 18:37:41 -0700

    Delete prisma/migrations/20240318063028_initial directory

--- prisma/migrations/20240318063028_initial/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `timeSpentEatinginMin` on the `Eaten` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Eaten" DROP COLUMN "timeSpentEatinginMin",
-ADD COLUMN     "price" INTEGER;
